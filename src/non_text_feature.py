579fbfe887cb6d11e65c60ccfb4703107eb8d726
jinfang <fangjx@dukes.jmu.edu>
2014-12-31 19:02:37 -0500

    add adaboost model for non_text data

--- src/non_text_feature.py
+++ src/non_text_feature.py
@@ -4,6 +4,11 @@ from sklearn import cross_validation
 from sklearn import svm, linear_model
 from sklearn.metrics import classification_report
 import json
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.datasets import make_gaussian_quantiles
+import numpy as np
 # from nltk import word_tokenize          
 # from nltk.stem import WordNetLemmatizer 
 # import nltk
@@ -129,6 +134,129 @@ def my_Logistic(X_train, X_test, y_train, y_test, C=1):
 	y_pre = [1 if it > -0.5 else 0 for it in y_pre]
 	return y_pre,classification_report(y_test, y_pre)
 
+def ada_boost(X_train, X_test, y_train, y_test, C=1):
+	# print " -- ada_boost Training ..."
+	# logreg = linear_model.LogisticRegression(C=C)
+	# logreg.fit(X_train, y_train)
+	# print " -- ada_boost Predicting ..."
+	# y_pre = logreg.predict(X_test)
+	# return y_pre, classification_report(y_test, y_pre)
+	# Construct dataset
+	# X1, y1 = make_gaussian_quantiles(cov=2.,
+	#                                  n_samples=200, n_features=2,
+	#                                  n_classes=2, random_state=1)
+	# X2, y2 = make_gaussian_quantiles(mean=(3, 3), cov=1.5,
+	#                                  n_samples=300, n_features=2,
+	#                                  n_classes=2, random_state=1)
+	
+	# count1=0
+	# count2 = 0
+	print(type(y_test))
+	# [ X1[count++], y1[count++] = X_train(i), y_train(i) for i in range(len(y_train)) if y_train(i)==1 ]
+	# for i in range(len(y_train.count)):
+	# 	if X_train[i]==1:
+	# 		X1[count1]  = X_train[i]
+	# 		y1[count1] = X_train[i] 
+	# 	else:
+	# 		X2[count2++]  = X_train[i]
+	X1 = []
+	X2 = []
+	y1 = []
+	y2 = []
+	for x, y in zip(X_train, y_train):
+		if y==1:
+			y1.append(y)
+			X1.append(x)
+		else:
+			y2.append(y)
+			X2.append(x)
+
+	# count = 0
+	# [ X2[count++], y2[count++]  = X_train(i), y_train(i) for i in range(len(y_train))if y_train(i)==0  ]
+	print(y1.count(1))
+
+	print(y2.count(0))
+	X1 =np.asarray(X1)
+	X2 =np.asarray(X2)
+	y1 = np.asarray(y1)
+	y2 = np.asarray(y2)
+	# y = np.asarray(y)
+	X = np.concatenate((X1, X2))
+	y = np.concatenate((y1, y2))
+
+	# Create and fit an AdaBoosted decision tree
+	bdt = AdaBoostClassifier(DecisionTreeClassifier(max_depth=1),
+	                         algorithm="SAMME",
+	                         n_estimators=200)
+
+	bdt.fit(X, y)
+
+	# plot_colors = "br"
+	# plot_step = 0.02
+	# class_names = "AB"
+
+	# plt.figure(figsize=(10, 5))
+
+	# Plot the decision boundaries
+	# plt.subplot(121)
+	# x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+	# y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	# xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+	#                      np.arange(y_min, y_max, plot_step))
+
+	# Z = bdt.predict(np.c_[xx.ravel(), yy.ravel()])
+	# Z = Z.reshape(xx.shape)
+	# cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+	# plt.axis("tight")
+
+	# Plot the training points
+	# for i, n, c in zip(range(2), class_names, plot_colors):
+	#     idx = np.where(y == i)
+	#     plt.scatter(X[idx, 0], X[idx, 1],
+	#                 c=c, cmap=plt.cm.Paired,
+	#                 label="Class %s" % n)
+	# plt.xlim(x_min, x_max)
+	# plt.ylim(y_min, y_max)
+	# plt.legend(loc='upper right')
+	# plt.xlabel('x')
+	# plt.ylabel('y')
+	# plt.title('Decision Boundary')
+
+	# Plot the two-class decision scores
+	twoclass_output = bdt.decision_function(X)
+
+	print(type(twoclass_output))
+
+	# import IPython
+	# IPython.embed()
+
+	# return twoclass_output, classification_report(y, twoclass_output)
+	# y_pre = bdt.decision_function(X_test)
+	y_pre = bdt.predict(X_test)
+	# y_pre = [1 if it > 0 else 0 for it in y_pre]
+
+	return y_pre,classification_report(y_test, y_pre)
+	# return y_pre,classification_report(y_test, y_pre)
+	# plot_range = (twoclass_output.min(), twoclass_output.max())
+	# plt.subplot(122)
+	# for i, n, c in zip(range(2), class_names, plot_colors):
+	    # plt.hist(twoclass_output[y == i],
+	    #          bins=10,
+	    #          range=plot_range,
+	    #          facecolor=c,
+	    #          label='Class %s' % n,
+	    #          alpha=.5)
+	# x1, x2, y1, y2 = plt.axis()
+	# plt.axis((x1, x2, y1, y2 * 1.2))
+	# plt.legend(loc='upper right')
+	# plt.ylabel('Samples')
+	# plt.xlabel('Score')
+	# plt.title('Decision Scores')
+
+	# plt.tight_layout()
+	# plt.subplots_adjust(wspace=0.35)
+	# plt.show()
+
 if __name__ == "__main__":
 	print "Extracting raw features ..."
 	user_info, labels = user_feature_extraction("../data/review_user.json", 500000)
@@ -136,13 +264,11 @@ if __name__ == "__main__":
 	print "Spliting data ..."
 	print len(user_info), len(labels)
 	user_info_train, user_info_test, y_train, y_test = data_split(user_info, labels)
-
+	# print(type(y_test))
+	# import IPython
+	# IPython.embed()
 	print "Training and Testing with logistic regression"
-	y_pre,report = my_Logistic(user_info_train, user_info_test, y_train, y_test, C=1)
+	y_pre,report = ada_boost(user_info_train, user_info_test, y_train, y_test, C=1)
 	print(report)
-
-
-
-
 
 
